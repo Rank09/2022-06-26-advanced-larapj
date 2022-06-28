4d9d24ab2c20e251045ed0d113bac2b5458fa09d
Masashi-Kikuchi <apex.13b.rew@gamail.com>
2022-06-28 22:19:50 +0900

    simplePaginate

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
-use App\Http\Controllers\BookController;//追記
+use Illuminate\Support\Facades\Route;//上部に追記
 
-Route::prefix('book')->group(function () {//以下を追記
-    Route::get('/', [BookController::class, 'index']);
-    Route::get('/add', [BookController::class, 'add']);
-    Route::post('/add', [BookController::class, 'create']);
-    Route::get('/relation', [AuthorController::class, 'relate']);
-});
+use App\Http\Controllers\SessionController;
+
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
